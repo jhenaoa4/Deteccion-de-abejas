5ec7ac952191530ae1158f4527c065afb41e392f
jhenaoa4 <41337763+jhenaoa4@users.noreply.github.com>
2018-07-17 12:05:18 -0500

    Add files via upload

--- talleres/taller01/Fecha.java
+++ talleres/taller01/Fecha.java
@@ -1,28 +1,48 @@
 public class Fecha {
-    public Fecha() {
-
+    private byte dia, mes;
+    private short anio;
+    public Fecha(byte dia, byte mes, short anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
     }
 
-    public int dia() {
-
+    public byte dia() {
+        return dia;
     }
 
-    public int mes() {
-
+    public byte mes() {
+        return mes;
     }
 
-    public int anio() {
-
+    public short anio() {
+        return anio;
     }
 
     // -1 si esta fecha es anterior a la otra
     // 0 si son iguales
     // 1 si esta fecha es posterior a la otra
     public int comparar(Fecha otra) {
-
+        if(anio > otra.anio()) {
+            return 1;
+        } else if(anio < otra.anio()){
+            return -1;
+        } else if (mes < otra.mes()){
+            return -1;
+        } else if (mes > otra.mes()){
+            return 1;
+        } else if (dia < otra.dia()){
+            return -1;
+        } else if (dia > otra.dia()){
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
+
     public String toString() {
-        
+        String fecha = dia+"/"+mes+"/"+anio;
+        return fecha;
     }
 }
--- talleres/taller01/Punto.java
+++ talleres/taller01/Punto.java
@@ -1,30 +1,35 @@
+import java.lang.Math;
 public class Punto {
-    private double x, y;
+    private int x, y;
 
-    public Punto() {
-
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public double x() {
-
+    public int x() {
+        return x;
     }
 
-    public double y() {
-
+    public int y() {
+        return y;
     }
 
     // http://mathworld.wolfram.com/PolarCoordinates.html
     public double radioPolar() {
-
+        double radioX = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+        return radioX;
     }
 
     // http://mathworld.wolfram.com/PolarCoordinates.html
     public double anguloPolar() {
-
+        double aP = Math.atan2(x,y);
+        return aP;
     }
 
     // http://mathworld.wolfram.com/Distance.html
     public double distanciaEuclidiana(Punto otro) {
-        
+        double d = Math.sqrt(Math.pow((otro.x()-x),2)+Math.pow((otro.y()-y),2));
+        return d;
     }
 }
