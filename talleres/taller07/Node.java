2866424c84821b316db642383125446befe4d2b9
jhenaoa4 <41337763+jhenaoa4@users.noreply.github.com>
2018-09-18 11:31:53 -0500

    Add files via upload

---
+++ talleres/taller07/Node.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Nodo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Node {
+public int data;
+public Node next;
+public Node(int data)
+{
+	next = null;
+	this.data = data;
+}
+	
+}
