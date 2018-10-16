d28fa9f031e170741dcc48620017568c62b5c488
jhenaoa4 <41337763+jhenaoa4@users.noreply.github.com>
2018-10-15 21:10:57 -0500

    Add files via upload

---
+++ laboratorios/lab04/codigo/BinaryTree.java
@@ -0,0 +1,52 @@
+public class BinaryTree {
+    public Node root;
+ 
+    public BinaryTree(){
+  root = null;
+ }
+    private int max2(int i, int j)
+    {
+        if (i > j)
+            return i;
+        return j;
+    }
+    
+    private int maxheightAUX(Node node)
+    {
+        if (node == null)
+                return 0;
+        else 
+                return max2(maxheightAUX(node.left), maxheightAUX(node.right))+1;
+    }
+    
+    public int maxheight()
+    {
+       return maxheightAUX(root);
+    }
+
+    private void recursivePrintAUX(Node node)
+    {
+        if (node != null)
+        {
+            recursivePrintAUX(node.left);
+            recursivePrintAUX(node.right);
+            System.out.println(node.data);
+        }
+        
+        
+    }
+    public void recursivePrint()
+    {
+        recursivePrintAUX(root);
+    }
+    
+    public static String abuelaMaterna(Node familiar){
+            Node mama = familiar.right;
+            Node abuela = mama.right;
+            return abuela.data;
+       }
+        
+
+    
+ 
+}
