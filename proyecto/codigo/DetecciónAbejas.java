6b9b7a46d8c849809b64ea27ab5519dd8395a874
jhenaoa4 <41337763+jhenaoa4@users.noreply.github.com>
2018-11-05 22:17:14 -0500

    Update DetecciónAbejas.java

--- proyecto/codigo/DetecciónAbejas.java
+++ proyecto/codigo/DetecciónAbejas.java
@@ -4,13 +4,20 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Stack;
 /**
- * Write a description of class DetecciónAbejas here.
+ * Clase para leer archivos con coordenadas de abejas y detectar cuáles de ellas están en riego de colisionar con cuáles 
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Juliana Henao & Gerónimo Zuluaga
+ * @version Noviembre 2018
  */
 public class DetecciónAbejas
 {
+    /**
+    * Metodo para aproximar la distancia entre dos abejas roboticas
+    *
+    * @param  abeja1  la primera abeja
+    * @param  abeja2  la segunda abeja
+    * @return la distancia aproximada entre las dos abejas, aproximando 1 grado como 111111 metros
+    */
     public static double distancia(Abeja abeja1, Abeja abeja2){
         return Math.sqrt(  Math.pow((abeja1.x - abeja2.x)*111111,2) +
             Math.pow((abeja1.y - abeja2.y)*111111,2) +
@@ -18,6 +25,12 @@ public class DetecciónAbejas
         );
     }
 
+    /**
+    * Metodo para leer un archivo de abejas y almacenarlas en un arreglo de tipo Abeja
+    *
+    * @param  numeroDeAbejas  El numero de abejas a leer
+    * @return un arreglo de Abeja donde cada elemento representa las coordenadas de una abeja
+    */
     public static Abeja[] leerArchivo(int numeroDeAbejas){
         final String nombreDelArchivo = "ConjuntoDeDatosCon"+numeroDeAbejas+"abejas.txt";
         Abeja[] arregloDeAbejas = new Abeja[numeroDeAbejas];
@@ -41,6 +54,12 @@ public class DetecciónAbejas
         return arregloDeAbejas;
     }
 
+    /**
+    * Metodo para crear una pila con un el arreglo de Abejas
+    *
+    * @param  arregloDeAbejas  Un conjunto de abejas a almacenar
+    * @return un Stack de Abeja donde cada elemento representa las coordenadas de una abeja
+    */
     public static Stack<Abeja> hacerPila(Abeja[] arregloDeAbejas){
         Stack <Abeja> abejas = new Stack();
         for(int i = 0; i < arregloDeAbejas.length; i++){
@@ -49,7 +68,13 @@ public class DetecciónAbejas
         return abejas;
     }
 
-    public static void colissionDetector(Abeja[] arregloDeAbejas, Stack <Abeja> abejas){
+    /**
+    * Metodo para detectar cuáles abejas estan en peligro de colisión
+    *
+    * @param  arregloDeAbejas  Un conjunto de abejas a evaluar
+    * @param  abejas una pila con abejas a evaluar
+    */
+    public static void collisionDetector(Abeja[] arregloDeAbejas, Stack <Abeja> abejas){
         while(abejas.size() != 0){
             Abeja actual = abejas.pop();
             for(int i = 0; i < arregloDeAbejas.length; i++){
@@ -65,7 +90,7 @@ public class DetecciónAbejas
         Abeja[] arregloDeAbejas = leerArchivo(100);
         Stack <Abeja> abejas = hacerPila(arregloDeAbejas);
         long startTime = System.currentTimeMillis();
-        colissionDetector(arregloDeAbejas,abejas);
+        collisionDetector(arregloDeAbejas,abejas);
         long estimatedTime = System.currentTimeMillis() - startTime;
         System.out.println("El algoritmo tomo un tiempo de: "+estimatedTime+" ms");
         }
